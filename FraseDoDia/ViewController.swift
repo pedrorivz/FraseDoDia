c9e576c8b86bccf4bb9c69c2c46278f8aa9a9b34
phsantosit <phsantosit@gmail.com>
2022-02-04 03:38:43 -0300

    Add interface and outlets/actions on components

--- FraseDoDia/ViewController.swift
+++ FraseDoDia/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var resultText: UITextView!
+    
+    
+    @IBAction func NovaFrase(_ sender: Any) {
+        var frases: [String] = []
+        frases.append("O Homem bom nunca gela")
+        frases.append("O homem ruim sempre emociona")
+        frases.append("E ainda não legalizaram a...")
+        
+        let numeroAleatorio = arc4random_uniform(3)
+        resultText.text = frases[Int(numeroAleatorio)]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
---
+++ README.md
@@ -0,0 +1,7 @@
+## DogsAge ##
+Aplicação que faz parte do curso da UDEMY 18 Apps em Udemy<br>
+Um app para te mostrar frases aleatórias<br>
+Melhorias mapeadas<br>
+<p>Consumir frases de uma API</p><br><br><br>
+<img src='screenshot.png'></img>
+
